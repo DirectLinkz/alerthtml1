43b7a0e2529d2e279e0e84135ca9e13f3fb7aa29
DirectLinkz <107291570+DirectLinkz@users.noreply.github.com>
2022-06-25 15:03:27 +0700

    Add files via upload

---
+++ alerthtml.js
@@ -0,0 +1,11 @@
+<script>
+function js_thml(){
+	let code = "";
+	code += "<div class=\"alert-wrap\"><div class=\"wsite-elements wsite-not-footer wsite-header-elements\"><div class=\"wsite-section-wrap\"><div class=\"wsite-section wsite-header-section wsite-section-bg-color\" style=\"height: auto;vertical-align: top;background-color: #0000FF;background-image: none;is_customized: 1;\" ><div class=\"wsite-section-content\"><div class=\"container\"><div class=\"alert\"><div class=\"wsite-section-elements\"><div><script language='javascript' src='//cdn.statically.io/gh/DirectLinkz/alert3/main/alert3.js'><\/script></div></div></div><!-- end container --></div><div class=\"\"></div></div></div></div></div><!-- end alert-wrap -->\n";
+	return code;
+}
+
+
+document.getElementById("js_html").innerText = js_thml();
+document.getElementById("html-text").value = js_thml();
+</script>
